62e959664d28fb564b7a24600e456da7d24f49f7
malkamat <rimka.matkalo@gmail.com>
2020-07-13 22:34:26 +0200

    correction algo 1

--- 1. NbdeVoyelle/app.js
+++ 1. NbdeVoyelle/app.js
@@ -1,26 +1,52 @@
-function nbDeVoyelles(txt) {
-  
+// ----------------- corrigé du cours --------------//
+// 1 ere methode par itération
+// // function nbDeVoyelles(txt) {
+// //   const voyelles = ["a","e","i","o","u","y"]
 
-}
+// //   let nbDeVoyelles = 0
+
+// //   for (let lettre of txt.toLowerCase()) {
+// //       if (voyelles.includes(lettre)) {
+// //           nbDeVoyelles++
+// //       }
+// //   }
+
+// //   return nbDeVoyelles
+
+// // }
+
+// 2 eme methode avec une REGEX
+
+ function nbDeVoyelles(txt) {
+  
+    let regexVoyelles = txt.match(/[aeiouy]/gi)
+    if (regexVoyelles) {
+
+        return regexVoyelles.length
+    } else {
+        return 0
+    }
+
+ }
 
 
 // ---------------------- ma réponse -------------- // // Réussi //
 
-function nbDeVoyelles(txt) {
-    let result = 0
+// // function nbDeVoyelles(txt) {
+// //     let result = 0
 
-    let tableau = [...txt.toLowerCase()]
-        for (i = 0; i < tableau.length; i++) {
-        if (tableau[i] === "a" || tableau[i] === "e" || tableau[i] === "i" || tableau[i] === "o" || tableau[i] === "u" || tableau[i] === "y") {
-            result += 1
+// //     let tableau = [...txt.toLowerCase()]
+// //         for (i = 0; i < tableau.length; i++) {
+// //         if (tableau[i] === "a" || tableau[i] === "e" || tableau[i] === "i" || tableau[i] === "o" || tableau[i] === "u" || tableau[i] === "y") {
+// //             result += 1
 
-        }
+// //         }
 
-    }
+// //     }
 
-    console.log(result)
+// //     console.log(result)
 
-}
+// // }
 
 
 

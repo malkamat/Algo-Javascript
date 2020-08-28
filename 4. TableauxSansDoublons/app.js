21e40d850ff09a08134ab64492df72a8b225d8bf
malkamat <rimka.matkalo@gmail.com>
2020-08-28 15:04:15 +0200

    algo 4 fini

--- 4. TableauxSansDoublons/app.js
+++ 4. TableauxSansDoublons/app.js
@@ -1,9 +1,33 @@
 function tableauxSansDoublons(...arrays) {
 
+    /*solution 2*/
 
+    let nvTab = []
 
+    arrays.forEach(item => {
+        nvTab = [...nvTab, ...item]
+    })
+console.log(nvTab)
+
+return Array.from(new StereoPannerNode(nvTab))
 }
 
+    /* solution 1 * /
+    // let nvTab =[]
+
+    // const arrDeArgs = Array.from(arguments)
+
+    // arrDeArgs.forEach(el => {
+    //     for(i = 0; i < el.length; i++) {
+    //         nvTab.push(el[i])
+    //     }
+    // })
+
+    // const tableauClean = nvTab.filter((item , index) => nvTab.indexOf(item) === index)
+
+    // return tableauClean
+}
+*/
 
 console.log(tableauxSansDoublons([5,5,4], [6,3,5])); // [5, 4, 6, 3]
 console.log(tableauxSansDoublons(["a","b","z"],["z","e","a"])); // ["a", "b", "z", "e"]

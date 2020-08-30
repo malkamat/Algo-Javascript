a44c07a298697843e7eadf8c49ab29ab56cd5fcc
malkamat <rimka.matkalo@gmail.com>
2020-08-30 14:32:05 +0200

    début algo 6

--- 6. lePLusFrequent/app.js
+++ 6. lePLusFrequent/app.js
@@ -1,6 +1,46 @@
 function lePlusFrequent(tab){
 
+    let myObject = {
 
+    }
+
+    let tabCopy = tab
+    let compteur = 0
+    let plusGrand = 0
+    let itemPlusNombreux = []
+
+    while(tabCopy.length > 0) {
+        for(i = 0 ; i < tabCopy.length; i++) {
+            if(tabCopy[0] === tabCopy[i]){
+                compteur++
+            }
+        }
+        myObject[`nbDe${tabCopy[0]}`] = [tabCopy[0], compteur]
+        compteur = 0
+
+        let varActuelle = tabCopy[0]
+        tabCopy = tabCopy.filter(item => item !== varActuelle)
+        console.log(myObject)
+
+        for(prop in myObject) {
+            if(myObject[prop][1] >= plusGrand) {
+                plusGrand = myObject[prop][1]
+            }
+        }
+
+        for(prop in myObject) {
+            if(myObject[prop][1] === 4) {
+                itemPlusNombreux.push(myObject[prop][0])
+            }
+        }
+
+        if(itemPlusNombreux.length > 1) {
+            return `Les éléments : ${itemPlusNombreux[0]} et ${itemPlusNombreux[1]} sont apparues le plus souvent, pour un total de ${plusGrand} fois chacun`
+        } else {
+            return `L'élément : ${itemPlusNombreux} est apparu le plus souvent, pour un total de ${plusGrand} fois chacun`
+        }
+       
+    }
 }
 
 

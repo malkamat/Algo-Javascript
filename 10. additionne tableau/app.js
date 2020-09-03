bf3d07cf42a624b3ffa99ca5b52c0d22fda74805
malkamat <rimka.matkalo@gmail.com>
2020-09-03 12:19:30 +0200

    algo 9 fini

--- 10. additionne tableau/app.js
+++ 10. additionne tableau/app.js
@@ -1,7 +1,13 @@
 function tableauAdition(tab) {
 
+    let count = 0
 
+   for(i = 0; i  < tab.length; i++) {
 
+    count += tab[i]
+   }
+
+   return count
 };
   
 console.log(tableauAdition([1,2,3,4,5,6]));
